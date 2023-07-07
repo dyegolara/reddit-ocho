b10cfea8dbe2cf1451971149393b15836ecffc26
dyegolara <dyegolara@icloud.com>
2023-07-07 09:54:38 -0600

    compact comment and its replies

--- src/components/Comment/index.tsx
+++ src/components/Comment/index.tsx
@@ -6,6 +6,7 @@ import CommentsList from "@/components/CommentsList";
 import AddReply from "@/components/AddReply";
 import { CommentType } from "@/types";
 import { useVotes } from "@/components/Votes/useVotes";
+import cn from "classnames";
 
 interface CommentProps {
   comment: CommentType;
@@ -15,6 +16,7 @@ const Comment: React.FC<CommentProps> = ({ comment }) => {
   const [replies, setReplies] = useState<{ data: CommentType }[]>([]);
   const { score, author, body, id } = comment;
   const { voted, handleVoteButtonClick, updatedScore } = useVotes(score);
+  const [compact, setCompact] = useState(false);
 
   useEffect(() => {
     if (comment.replies) {
@@ -29,24 +31,29 @@ const Comment: React.FC<CommentProps> = ({ comment }) => {
   return (
     <div className={styles.comment}>
       <div className={styles.commentVotes}>
-        <div className={styles.threadlineWrapper}>
+        <div
+          className={styles.threadlineWrapper}
+          onClick={() => setCompact(!compact)}
+        >
           <div className={styles.threadline} />
         </div>
       </div>
       <div className={styles.commentContent}>
         <span className={styles.author}>u/{author}</span>
         <span className={styles.points}> · {updatedScore} points</span>
-        <p className={styles.body}>{body}</p>
-        <div className={styles.commentActions}>
-          <Votes
-            score={updatedScore}
-            onVote={handleVoteButtonClick}
-            voted={voted}
-            horizontal={true}
-          />
-          <AddReply setReplies={setReplies} />
+        <div className={cn({ [styles.compact]: compact })}>
+          <div className={styles.commentActions}>
+            <p className={styles.body}>{body}</p>
+            <Votes
+              score={updatedScore}
+              onVote={handleVoteButtonClick}
+              voted={voted}
+              horizontal={true}
+            />
+            <AddReply setReplies={setReplies} />
+          </div>
+          <CommentsList comments={replies} />
         </div>
-        <CommentsList comments={replies} />
       </div>
     </div>
   );
