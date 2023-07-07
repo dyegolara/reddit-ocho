8816fafe99e1ae78767a76bbf85d9d79244ecc58
dyegolara <dyegolara@icloud.com>
2023-07-06 23:15:15 -0600

    add response type

--- src/app/r/[[...permalink]]/page.tsx
+++ src/app/r/[[...permalink]]/page.tsx
@@ -3,20 +3,22 @@ import Loading from "@/components/Loading";
 import { Container } from "@/components/Container";
 import Post from "@/components/Post";
 import { getData } from "@/utils";
-import AddComment from "@/components/AddComment";
-import CommentsList from "@/components/CommentsList";
 import PostComments from "@/app/r/[[...permalink]]/PostComments";
+import { CommentType, PostType } from "@/types";
 
 export default async function Detail({
   params: { permalink },
 }: {
   params: { permalink: string[] };
 }) {
-  const [rawPost, rawComments] = await getData(
-    `https://www.reddit.com/r/${permalink.join("/")}.json`
-  );
+  const [rawPost, rawComments]: [
+    { data: { children: { data: PostType }[] } },
+    { data: { children: { data: CommentType }[] } }
+  ] = await getData(`https://www.reddit.com/r/${permalink.join("/")}.json`);
+
   const [post] = rawPost?.data?.children;
   const comments = rawComments?.data?.children || [];
+
   return (
     <div className={styles.detail}>
       <Container>
