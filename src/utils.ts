93290719e6bf6fd1190c8219abf3c64e12902328
dyegolara <dyegolara@icloud.com>
2025-10-16 13:56:58 -0600

    log response again

--- src/utils.ts
+++ src/utils.ts
@@ -1,8 +1,8 @@
 export const getData = async (url: string) => {
   const response = await fetch(url);
+  console.log("response", response);
   if (!response.ok) {
     return null;
   }
-  console.log("response", response);
   return await response.json();
 };
