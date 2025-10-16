e09b4ed99264ce0f13a669442eed38011667b333
dyegolara <dyegolara@icloud.com>
2025-10-16 13:58:57 -0600

    update headers

--- src/utils.ts
+++ src/utils.ts
@@ -1,6 +1,12 @@
 export const getData = async (url: string) => {
-  const response = await fetch(url);
-  console.log("response", response);
+  const response = await fetch(url, {
+    headers: {
+      "User-Agent": "reddit-ocho-app/1.0",
+      Accept: "application/json",
+    },
+    cache: "no-store",
+  });
+  console.log({ response });
   if (!response.ok) {
     return null;
   }
