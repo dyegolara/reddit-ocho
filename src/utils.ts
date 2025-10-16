cf893e09f197c0933e3afd559f88d81094f665c1
dyegolara <dyegolara@icloud.com>
2025-10-16 14:02:06 -0600

    more headers

--- src/utils.ts
+++ src/utils.ts
@@ -1,8 +1,15 @@
 export const getData = async (url: string) => {
   const response = await fetch(url, {
     headers: {
-      "User-Agent": "reddit-ocho-app/1.0",
+      "User-Agent":
+        "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/119.0.0.0 Safari/537.36",
       Accept: "application/json",
+      "Accept-Language": "en-US,en;q=0.9",
+      "Accept-Encoding": "gzip, deflate, br",
+      Referer: "https://www.reddit.com/",
+      "Sec-Fetch-Dest": "empty",
+      "Sec-Fetch-Mode": "cors",
+      "Sec-Fetch-Site": "same-origin",
     },
   });
   console.log({ response });
